cf3d11b3cf94fa223bca748754ef5fd4312c2e47
Christopher Roberts <chrisrt@microsoft.com>
2020-08-11 16:13:03 -0700

    Initial VM Scale Set and Storage Account configuration in Terraform

--- main.tf
+++ main.tf
@@ -1,12 +1,129 @@
-# Define the provider, version and feature set 
 provider "azurerm" {
-  version="~>2.0"
   features {}
 }
 
-# Create a resource group based on the prefix variable
-# Note that here "main" is an alias for future use
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
   location = var.location
+}
+
+resource "azurerm_storage_account" "datatier" {
+  name                     = "${var.prefix}storage"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.1.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "main" {
+  name                 = "main"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.1.2.0/24"]
+}
+
+resource "azurerm_public_ip_prefix" "main" {
+  name                = "${var.prefix}-pip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_windows_virtual_machine_scale_set" "main" {
+  name                 = "${var.prefix}vmss"
+  resource_group_name  = azurerm_resource_group.main.name
+  location             = azurerm_resource_group.main.location
+  sku                  = "Standard_F2"
+  instances            = 2
+  admin_username       = "adminuser"
+  admin_password       = var.admin_password
+  computer_name_prefix = var.prefix
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  network_interface {
+    name    = "${var.prefix}nic"
+    primary = true
+
+    ip_configuration {
+      name      = "internal"
+      primary   = true
+      subnet_id = azurerm_subnet.main.id
+
+      public_ip_address {
+        name                = "${var.prefix}pip"
+        public_ip_prefix_id = azurerm_public_ip_prefix.main.id
+      }
+    }
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+}
+
+
+resource "azurerm_subnet_network_security_group_association" "main" {
+  depends_on=[azurerm_network_security_group.main]
+  subnet_id                 = azurerm_subnet.main.id
+  network_security_group_id = azurerm_network_security_group.main.id
+}
+
+resource "azurerm_network_security_group" "main" {
+  depends_on=[azurerm_resource_group.main]
+
+  name = "${var.prefix}-vmss-nsg"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "allow-rdp"
+    description                = "allow-rdp"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow-http"
+    description                = "allow-http"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "allow-https"
+    description                = "allow-https"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "Internet"
+    destination_address_prefix = "*"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,34 @@
 variable "prefix" {
-  description = "The prefix used for all resources in this example"
+    description = "A prefix label, like wwtdev"
 }
 
 variable "location" {
-  description = "The Azure location where all resources in this example should be created"
+ description = "The location where resources will be created"
+}
+
+variable "tags" {
+ description = "A map of the tags to use for the resources that are deployed"
+ type        = map(string)
+
+ default = {
+   environment = "dev"
+ }
+}
+
+variable "resource_group_name" {
+ description = "The name of the resource group in which the resources will be created"
+}
+
+variable "application_port" {
+   description = "The port that you want to expose to the external load balancer"
+   default     = 80
+}
+
+variable "admin_user" {
+   description = "User name to use as the admin account on the VMs that will be part of the VM Scale Set"
+   default     = "azureuser"
+}
+
+variable "admin_password" {
+   description = "Default password for admin account"
 }
