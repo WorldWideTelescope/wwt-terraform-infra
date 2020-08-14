1aaf731d38fbe60da97b127863baab986827b778
Christopher Roberts <chrisrt@microsoft.com>
2020-08-14 08:58:43 -0700

    Added a vmss extension to the terraform main script that will install IIS when VM instances are upgraded after creation

--- main.tf
+++ main.tf
@@ -127,3 +127,18 @@ resource "azurerm_network_security_group" "main" {
     destination_address_prefix = "*"
   }
 }
+
+resource "azurerm_virtual_machine_scale_set_extension" "vm_extension_install_iis" {
+  name                       = "vm_extension_install_iis"
+  virtual_machine_scale_set_id = azurerm_windows_virtual_machine_scale_set.main.id
+  publisher                  = "Microsoft.Compute"
+  type                       = "CustomScriptExtension"
+  type_handler_version       = "1.8"
+  auto_upgrade_minor_version = true
+
+  settings = <<SETTINGS
+    {
+        "commandToExecute": "powershell -ExecutionPolicy Unrestricted Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools"
+    }
+SETTINGS
+}
