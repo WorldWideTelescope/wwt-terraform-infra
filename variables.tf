44b260e444453e8af55e6833ae2a8fc55aa15d8a
Peter Williams <peter@newton.cx>
2020-09-30 17:14:03 -0400

    variables.tf: remove unused variable, homogenize indentation

--- variables.tf
+++ variables.tf
@@ -3,9 +3,5 @@ variable "prefix" {
 }
 
 variable "location" {
- description = "The location where resources will be created"
-}
-
-variable "resource_group_name" {
- description = "The name of the resource group in which the resources will be created"
+    description = "The location where resources will be created"
 }
