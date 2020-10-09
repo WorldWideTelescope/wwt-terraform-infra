c9f53c178435b7e3f6933ddbb8d4e3292c9fee11
Chris Roberts <chrisrt@microsoft.com>
2020-10-09 11:43:31 -0700

    Configured windows VM

--- etl/variables.tf
+++ etl/variables.tf
@@ -10,6 +10,10 @@ variable "admin_username" {
    description = "Default username for admin account"
 }
 
+variable "admin_password" {
+   description = "The password for the default admin user"
+}
+
 variable "vm_sku" {
    description = "Default VM size"
    default = "Standard_A2"
