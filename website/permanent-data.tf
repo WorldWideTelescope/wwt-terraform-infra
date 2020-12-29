7502318ce215bcc3cce85cedd3f470b1fb110888
Peter Williams <peter@newton.cx>
2020-12-29 13:06:43 -0500

    website: tell Terraform about the existing "wwtweb" storage account

--- website/permanent-data.tf
+++ website/permanent-data.tf
@@ -5,3 +5,12 @@ resource "azurerm_resource_group" "permanent_data" {
   name     = "permanent-data"
   location = var.location
 }
+
+resource "azurerm_storage_account" "permanent_data_wwtweb" {
+  name                      = var.legacyNameWwtweb
+  resource_group_name       = azurerm_resource_group.permanent_data.name
+  location                  = azurerm_resource_group.permanent_data.location
+  account_tier              = "Standard"
+  account_replication_type  = "GRS"
+  enable_https_traffic_only = false
+}
--- website/variables.tf
+++ website/variables.tf
@@ -1,10 +1,21 @@
+// Fundamentals
+
 variable "prefix" {
-  description = "A prefix label, like wwtdev"
+  description = "A prefix label, like wwtdev, for uniquifying public resource names"
 }
 
 variable "location" {
   description = "The location where resources will be created"
 }
+
+// Names for "legacy" resources -- preexisting assets that we have imported into
+// Terraform.
+
+variable "legacyNameWwtweb" {
+  description = "The name to use for the 'legacy' storage account equivalent to 'wwtweb' in production"
+}
+
+// Other configuration.
 
 variable "liveClientId" {
   description = "The ID of the Microsoft Live OAuth app"
