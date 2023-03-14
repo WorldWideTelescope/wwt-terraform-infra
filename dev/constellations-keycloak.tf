f6d4414fef99970bb755b281dab8198ec5419a0f
Peter Williams <peter@newton.cx>
2023-03-14 13:13:03 -0400

    dev: try adding private DNS records for the Postgres server

--- dev/constellations-keycloak.tf
+++ dev/constellations-keycloak.tf
@@ -38,7 +38,7 @@ resource "azurerm_private_endpoint" "cx_backend_sql" {
 
   private_dns_zone_group {
     name                 = "default"
-    private_dns_zone_ids = [azurerm_private_dns_zone.cx_backend.id]
+    private_dns_zone_ids = [azurerm_private_dns_zone.cx_sql.id]
   }
 
   private_service_connection {
@@ -47,4 +47,19 @@ resource "azurerm_private_endpoint" "cx_backend_sql" {
     is_manual_connection           = false
     subresource_names              = ["postgresqlServer"]
   }
+}
+
+# Private DNS zone for the SQL stuff
+
+resource "azurerm_private_dns_zone" "cx_sql" {
+  name                = "privatelink.postgres.database.azure.com"
+  resource_group_name = azurerm_resource_group.cx_backend.name
+}
+
+resource "azurerm_private_dns_a_record" "cx_backend_sql" {
+  name                = "${var.prefix}-cxsql"
+  zone_name           = azurerm_private_dns_zone.cx_sql.name
+  resource_group_name = azurerm_resource_group.cx_backend.name
+  ttl                 = 10
+  records             = ["10.0.4.4"]
 }
