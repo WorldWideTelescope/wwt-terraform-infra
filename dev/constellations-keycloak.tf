f5d7cb68ca67cdfc93f08349e0433d2c88af3877
Peter Williams <peter@newton.cx>
2023-03-14 13:01:11 -0400

    dev: sigh, private network stuff requires a non-Basic server; go for it

--- dev/constellations-keycloak.tf
+++ dev/constellations-keycloak.tf
@@ -7,18 +7,44 @@ resource "azurerm_postgresql_server" "cxsql" {
   location            = azurerm_resource_group.cx_backend.location
   resource_group_name = azurerm_resource_group.cx_backend.name
 
-  sku_name                     = "B_Gen5_1"
+  sku_name                     = "GP_Gen5_2"
   version                      = "11"
   storage_mb                   = 5120
   backup_retention_days        = 7
   geo_redundant_backup_enabled = false
   auto_grow_enabled            = false
 
-  public_network_access_enabled     = true # basic tier requires this (?!)
+  public_network_access_enabled     = false
   ssl_enforcement_enabled           = true
   ssl_minimal_tls_version_enforced  = "TLS1_2"
   infrastructure_encryption_enabled = false
 
   administrator_login          = "psqladmin"
   administrator_login_password = var.cxsqlAdminPassword
+}
+
+resource "azurerm_subnet" "cx_backend_sql" {
+  name                 = "${var.prefix}-cxbeSqlSubnet"
+  resource_group_name  = azurerm_resource_group.cx_backend.name
+  virtual_network_name = azurerm_virtual_network.cx_backend.name
+  address_prefixes     = ["10.0.4.0/24"]
+}
+
+resource "azurerm_private_endpoint" "cx_backend_sql" {
+  name                = "${var.prefix}-cxbeSqlEndpoint"
+  location            = azurerm_resource_group.cx_backend.location
+  resource_group_name = azurerm_resource_group.cx_backend.name
+  subnet_id           = azurerm_subnet.cx_backend_sql.id
+
+  private_dns_zone_group {
+    name                 = "default"
+    private_dns_zone_ids = [azurerm_private_dns_zone.cx_backend.id]
+  }
+
+  private_service_connection {
+    name                           = "${var.prefix}-cxbeSqlEndpoint"
+    private_connection_resource_id = azurerm_postgresql_server.cxsql.id
+    is_manual_connection           = false
+    subresource_names              = ["postgresqlServer"]
+  }
 }
